472b89a6a4089d56e9ea46c2936af1035af468ce
rzentenoz <rodrigo.zenteno@agile.cl>
2017-03-22 16:40:56 -0300

    22/03/2017 con comentarios

--- 22032017/clase.c
+++ 22032017/clase.c
@@ -1,14 +1,22 @@
+/*
+Autor: Rodrigo Zenteno
+Descripción: Programa que calcula la sumatoria de 3 números, en el que cada número aceptado por entrada estandar debe ser par y estar dentro del rango 0 a 9
+ */
 #include <stdio.h>
 
 int main(){
-	int i, cont, sum = 0;	
+	int i, cont, sum = 0; // Declaración múltiple de variables 
+	// Inicio de ciclo de 3 repeticiones
 	for(cont = 0;cont < 3; cont++){
+		// Inicio del ciclo de solicitud de número con restricciones de aceptación
 		do{
 			printf("Ingrese el elemento %d: ", cont+1);
 			scanf("%d",&i);		
 		}while(i < 0 || i > 9 || i % 2 );	
-		sum += i;
+		// Fin del ciclo de solicitud de número con restricciones de aceptación
+		sum += i; // Se añade el valor tomado desde la entrada estándar al total de la sumatoria
 	}
+	// Fin de ciclo de 3 repeticiones
 	printf("La sumatoria es %d\n", sum);
 	return 0;
 }
